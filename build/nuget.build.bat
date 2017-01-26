86a5dc35795670e2a0270b187104a47ae8298def
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-01-26 14:16:54 -0800

    Ensure the correct build configuration is used when creating the NuGet package.

--- build/nuget.build.bat
+++ build/nuget.build.bat
@@ -3,6 +3,7 @@ REM It is run by the Visual Studio buld process.
 
 SET NUGET=..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe
 SET VER=%1
+SET CONFIG=%2
 
 @ECHO ====== Creating NuGet Package in "dist" Folder
-%NUGET% pack Rhythm.Caching.Core.csproj -outputdirectory ..\..\dist -version %VER%
+%NUGET% pack Rhythm.Caching.Core.csproj -outputdirectory ..\..\dist -version %VER% -Properties Configuration=%CONFIG%
