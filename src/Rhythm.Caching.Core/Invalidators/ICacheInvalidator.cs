717d781bcadb21cf9d80388097845962de7a1fef
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-01-26 14:13:18 -0800

    Added ICacheInvalidator.

---
+++ src/Rhythm.Caching.Core/Invalidators/ICacheInvalidator.cs
@@ -0,0 +1,30 @@
+﻿namespace Rhythm.Caching.Core.Invalidators
+{
+
+    // Namespaces.
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface to be used by invalidators that apply to instance caches.
+    /// </summary>
+    public interface ICacheInvalidator
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Invalidates the cache unconditionally.
+        /// </summary>
+        void Invalidate();
+
+        /// <summary>
+        /// Invalidates if the invalidator matches any of the specified content type aliases.
+        /// </summary>
+        /// <param name="aliases"></param>
+        void InvalidateForAliases(IEnumerable<string> aliases);
+
+        #endregion
+
+    }
+
+}
