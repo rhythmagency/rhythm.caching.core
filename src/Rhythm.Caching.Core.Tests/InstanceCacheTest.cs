311cdeacabd82e0679048ba5451cda872433a153
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2020-01-24 17:38:51 -0800

    feat (Testing) add tests for InstanceCache

---
+++ src/Rhythm.Caching.Core.Tests/InstanceCacheTest.cs
@@ -0,0 +1,85 @@
+﻿namespace Rhythm.Caching.Core.Tests
+{
+
+    // Namespaces.
+    using System;
+    using System.Threading;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Rhythm.Caching.Core.Caches;
+
+    /// <summary>
+    /// Tests for InstanceCache.
+    /// </summary>
+    [TestClass]
+    public class InstanceCacheTest
+    {
+
+        #region Properties
+
+        private TimeSpan CacheDuration => TimeSpan.FromHours(1);
+        private TimeSpan TwoSeconds => TimeSpan.FromSeconds(2);
+
+        #endregion
+
+        #region Test Methods
+
+        /// <summary>
+        /// Test to ensure you get the correct value from the cache.
+        /// </summary>
+        [TestMethod]
+        public void GetValueTest()
+        {
+            var cache = new InstanceCache<string>();
+            var value = cache.Get(CacheDuration, () =>
+            {
+                return "Hello";
+            });
+            Assert.AreEqual("Hello", value);
+        }
+
+        /// <summary>
+        /// Test to ensure the cache expires in the expected amount of time.
+        /// </summary>
+        [TestMethod]
+        public void CacheExpiresOnTimeTest()
+        {
+            var cache = new InstanceCache<string>(TwoSeconds);
+            var startTime = DateTime.Now;
+            var value = cache.Get(CacheDuration, () =>
+            {
+                var result = default(string);
+                var thread = new Thread(new ThreadStart(() =>
+                {
+                    result = cache.Get(CacheDuration, () =>
+                    {
+                        return "Hello";
+                    });
+                }));
+                thread.Start();
+                thread.Join();
+                return result;
+            });
+            var endTime = DateTime.Now;
+            var diff = endTime.Subtract(startTime);
+            Assert.IsNull(value);
+            Assert.IsTrue(diff.TotalSeconds >= 1.8 && diff.TotalSeconds < 3);
+        }
+
+        /// <summary>
+        /// Test to ensure the original cached value is returned rather than a newly supplied value.
+        /// </summary>
+        [TestMethod]
+        public void GetCachedValueTest()
+        {
+            var cache = new InstanceCache<string>();
+            var firstValue = "Hello";
+            var value = cache.Get(CacheDuration, () => firstValue);
+            value = cache.Get(CacheDuration, () => "World");
+            Assert.AreEqual("Hello", value);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Rhythm.Caching.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rhythm.Caching.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rhythm.Caching.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d6f5d21-f8fe-419e-a3fd-e77770cfa6c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
